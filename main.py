e2982e2f54b124bbab7a0169d4f0518e4cacf0bf
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-10-04 16:40:34 -0700

    finished with format output file

--- main.py
+++ main.py
@@ -17,21 +17,27 @@ conf.setMaster('local[4]')
 # Load and parse the data
 data = sc.textFile('./train.dat')
 ratings = data.map(lambda line: line.split('\t'))\
-    .map(lambda line: Rating(int(line[0]), int(line[1]), float(line[2])))
+    .map(lambda rating: Rating(int(rating[0]), int(rating[1]), float(rating[2])))
 
 # Build the recommendation model using Alternating Least Squares
 rank = 10
-numIterations = 10
-model = ALS.train(ratings, rank, numIterations)
+num_iterations = 10
+model = ALS.train(ratings, rank, num_iterations)
 
 # Evaluate the model on training data
-testdata = sc.textFile('./test.dat')
-testratings = testdata.map(lambda line: line.split('\t'))\
+test_data = sc.textFile('./test.dat')
+test_ratings = test_data.map(lambda line: line.split('\t'))
 
-predictions = model.predictAll(testratings.map(lambda d: (int(d[0]), int(d[1]))))
+predictions = model.predictAll(test_ratings.map(lambda d: (int(d[0]), int(d[1]))))
 
 # Write the RDD to format.dat file, where each row is a score prediction
-predictions.map(lambda rating: int(rating[3])) \
-    .saveAsTextFile('./format.dat')
+format_file = open('./format.dat','w')
+
+for prediction in predictions.collect():
+    if (int(prediction[2]) < 0):
+        format_file.write('{}\n'.format(int(0)))
+    format_file.write('{}\n'.format(prediction[2]))
+
+format_file.close()
 
 sc.stop()
